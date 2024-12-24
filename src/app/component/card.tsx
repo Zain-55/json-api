e26085bab2896ffb00f9bc155f36e7c25569de5e
Zain-55 <zain4u123@gmail.com>
2024-12-24 22:18:08 +0500

    repoupdate

---
+++ src/app/component/card.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
+function card() {
+  return (
+    <main>
+      <button>Home</button>
+      <button>About</button>
+      <button>Product</button>
+
+    </main>
+  )
+}
+
+export default card
